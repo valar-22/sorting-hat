179cafd0a343947d76fe8ac7a5072418e0835f93
valar-22 <vicente@alarconycia.cl>
2026-02-05 21:47:30 -0300

    feat: Implement LeetCode3379 solution.

--- leetcode/3379. Transformed Array/3379.cpp
+++ leetcode/3379. Transformed Array/3379.cpp
@@ -1,7 +1,37 @@
 #include <vector>
 using namespace std;
 
+/**
+ * Constructs a transformed array result where result[i] is the element at
+ * index (i + nums[i]) in the circular array nums.
+ * - If nums[i] > 0, moves right.
+ * - If nums[i] < 0, moves left.
+ * - If nums[i] == 0, stays at the current index.
+ *
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ */
+
 class Solution {
 public:
-  vector<int> constructTransformedArray(vector<int> &nums) {}
+  vector<int> constructTransformedArray(vector<int> &nums) {
+
+    int size = nums.size();
+    vector<int> ans(size, 0);
+
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] > 0) {
+        ans[i] = nums[(i + nums[i]) % size];
+      }
+      if (nums[i] < 0) {
+        int index = (i + nums[i]) % size;
+        if (index < 0) {
+          ans[i] = nums[size + index];
+        } else {
+          ans[i] = nums[index];
+        }
+      }
+    }
+    return ans;
+  }
 };
